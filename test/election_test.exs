c398f64fde9a799b585c4ae94a377c2c57fe7261
Maxon Tayebwa <maxtayebwa@gmail.com>
2021-06-03 10:30:03 +0300

    fix failing tests

--- test/election_test.exs
+++ test/election_test.exs
@@ -14,11 +14,38 @@ defmodule ElectionTest do
     assert election == %Election{name: "Will Ferrell"}
   end
 
-  test "adding a new candidate from a command"
+  test "adding a new candidate from a command", ctx do
+    command = "add Will Ferrell"
+    election = Election.update(ctx.election, command)
 
-  test "voting for a candidate from a command"
+    assert election == %Election{
+             candidates: [%Candidate{id: 1, name: "Will Ferrell"}],
+             next_id: 2
+           }
+  end
 
-  test "invalid command"
+  test "voting for a candidate from a command", ctx do
+    add_command = "add Will Ferrell"
+    election = Election.update(ctx.election, add_command)
 
-  test "quitting the app"
+    vote_command = "vote 1"
+
+    assert election |> Election.update(vote_command) == %Election{
+             candidates: [%Candidate{id: 1, name: "Will Ferrell", votes: 1}],
+             next_id: 2
+           }
+  end
+
+  test "invalid command", ctx do
+    invalid_command = "invalida_command"
+    election = Election.update(ctx.election, invalid_command)
+    assert election == ctx.election
+  end
+
+  test "quitting the app", ctx do
+    quit_command = "q"
+    election = Election.update(ctx.election, quit_command)
+
+    assert election == :quit
+  end
 end
